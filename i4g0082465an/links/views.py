79d42c60131dca0a1c7bc15085d39f1e1383c767
Knowledge <megamindtheincredible@gmail.com>
2022-07-17 18:43:28 +0100

    first-commit

---
+++ i4g0082465an/links/views.py
@@ -0,0 +1,58 @@
+from rest_framework.generics import ListAPIView
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import UpdateAPIView
+from rest_framework.generics import DestroyAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Link
+from .serializers import LinkSerializer
+from django.utils import timezone
+import datetime
+
+
+class PostListApi(ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostCreateApi(CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDetailApi(RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostUpdateApi(UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDeleteApi(DestroyAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class ActiveLinkView(APIView):
+    """
+    Returns a list of all active (publicly accessible) links
+    """
+    def get(self, request):
+        """ 
+        Invoked whenever a HTTP GET Request is made to this view
+        """
+        qs = Link.public.all()
+        data = LinkSerializer(qs, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
+    
+class RecentLinkView(APIView):
+    """
+    Returns a list of recently created active links
+    """
+    def get(self, request):
+        """ 
+        Invoked whenever a HTTP GET Request is made to this view
+        """
+        seven_days_ago = timezone.now() - datetime.timedelta(days=7)
+        qs = Link.public.filter(created_date__gte=seven_days_ago)
+        data = LinkSerializer(qs, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
